756e36203f5a70674aeb7fe8f457a8b9f4875200
abhijitCN <abhijitsaha@capitalnumbers.com>
2022-12-28 12:26:44 +0530

    error solve

--- Web_App/client/src/App.tsx
+++ Web_App/client/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import NavBar from './components/navBar';
+import NavBar from './components/NavBar';
 function App() {
     return (
         <div className="App">
