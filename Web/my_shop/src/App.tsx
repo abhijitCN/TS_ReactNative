9a06665b8a1b91d4db3da57348f7b8ff98c17046
abhijitCN <abhijitsaha@capitalnumbers.com>
2023-02-17 14:28:57 +0530

    first web change

---
+++ Web/my_shop/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>My Shop</p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
